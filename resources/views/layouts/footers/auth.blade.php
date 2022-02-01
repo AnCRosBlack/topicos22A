ee303b673627634e69f4808b5c09084ab6e55d42
Antonio Chavarin Rosales <91075001+antonio-chavarin@users.noreply.github.com>
2022-01-31 18:59:04 -0600

    inicial commit

---
+++ resources/views/layouts/footers/auth.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>, Hecho con <i class="material-icons">favorite</i> por
+      <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a> y <a href="https://www.updivision.com" target="_blank">UPDIVISION</a> para una mejor web.
+    </div>
+  </div>
+</footer>
